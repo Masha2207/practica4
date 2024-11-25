15023319bf670d7376403e12b632c5562adbf5c5
MshKa <komaygorodskaya00@mail.ru>
2024-11-25 15:19:49 +0900

    Добавьте файлы проекта.

---
+++ practica4/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace ListViewExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Изображение = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Имя = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Фамилия = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Изображение,
+            this.Имя,
+            this.Фамилия});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(2, -1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(685, 428);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Изображение
+            // 
+            this.Изображение.Text = "Изображение";
+            this.Изображение.Width = 114;
+            // 
+            // Имя
+            // 
+            this.Имя.Text = "Имя";
+            this.Имя.Width = 94;
+            // 
+            // Фамилия
+            // 
+            this.Фамилия.Text = "Фамилия";
+            this.Фамилия.Width = 105;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(493, 182);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(493, 239);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(493, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Изображение;
+        private System.Windows.Forms.ColumnHeader Имя;
+        private System.Windows.Forms.ColumnHeader Фамилия;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ practica4/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ListViewExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCustomComponents();
+            LoadData();
+        }
+
+        private void InitializeCustomComponents()
+        {
+            // Создание ImageList для хранения изображений
+            listView1.SmallImageList = new ImageList();
+            listView1.SmallImageList.ImageSize = new Size(50, 50);
+
+            // Добавление столбцов в ListView
+            listView1.Columns.Add("Изображение", 100);
+            listView1.Columns.Add("Имя", 100);
+            listView1.Columns.Add("Фамилия", 100);
+
+            // Кнопка для добавления нового элемента
+            button1.Text = "Добавить";
+            button1.Click += AddNewItem;
+
+            // Кнопка для выбора изображения
+            Button btnAddImage = new Button
+            {
+                Location = new Point(400, 300),
+                Text = "Выбрать изображение"
+            };
+            btnAddImage.Click += BtnAddImage_Click;
+            Controls.Add(btnAddImage);
+        }
+
+        private void BtnAddImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Изображения|*.png;*.jpg;*.jpeg;*.bmp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Загрузка выбранного изображения
+                    Image img = Image.FromFile(openFileDialog.FileName);
+                    listView1.SmallImageList.Images.Clear();
+                    listView1.SmallImageList.Images.Add(img);
+                }
+            }
+        }
+
+        private void LoadData()
+        {
+            // Добавление начальных данных
+            AddItem("Иван", "Иванов", "cat1.jpg");
+        }
+
+        private void AddNewItem(object sender, EventArgs e)
+        {
+            // Получение данных из текстовых полей
+            string firstName = textBox1.Text;
+            string lastName = textBox2.Text;
+
+            if (string.IsNullOrWhiteSpace(firstName) || string.IsNullOrWhiteSpace(lastName))
+            {
+                MessageBox.Show("Введите имя и фамилию.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Добавление нового элемента в ListView
+            AddItem(firstName, lastName, "cat.jpg"); // Используем стандартное изображение, можно изменить
+        }
+
+        private void AddItem(string firstName, string lastName, string imagePath)
+        {
+            // Загружаем изображение
+            Image image;
+            try
+            {
+                image = Image.FromFile(imagePath);
+            }
+            catch (Exception)
+            {
+                image = new Bitmap(50, 50);
+                using (Graphics g = Graphics.FromImage(image))
+                {
+                    g.Clear(Color.White);
+                }
+            }
+
+            // Добавляем изображение в ImageList
+            int imageIndex = listView1.SmallImageList.Images.Count;
+            listView1.SmallImageList.Images.Add(image);
+
+            // Создание элемента для ListView
+            ListViewItem item = new ListViewItem("", imageIndex)
+            {
+                SubItems = { firstName, lastName }
+            };
+
+            // Добавление элемента в ListView
+            listView1.Items.Add(item);
+        }
+
+        
+    }
+}
